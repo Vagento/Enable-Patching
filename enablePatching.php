34826b1a14ca90b23daef13581b69c7f4d3de911
Walter Woshid <36635504+WalterWoshid@users.noreply.github.com>
2021-12-03 00:08:42 +0100

    Updated enable patching script
    Moved unload file after isset, so it can unload itself, even if 'enable-patching' is already set.

--- enablePatching.php
+++ enablePatching.php
@@ -7,21 +7,23 @@ if (!isset($composerJson['extra']['enable-patching']) || $composerJson['extra'][
     $composerJson['extra']['enable-patching'] = true;
     file_put_contents('composer.json', json_encode($composerJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
 
+    // "vagento/enable-patching" package adds {"extra": {"enable-patching": true}} to composer.json.
+    // Please run composer update/install again.
     if (php_sapi_name() === 'cli') echo "\e[33m"; // Yellow
-    echo "\"vagento/enable-patching\" package requires ";
+    echo "\"vagento/enable-patching\" package adds ";
     if (php_sapi_name() === 'cli') echo "\e[36m"; // Cyan
-    echo "{\"extra\": {\"enable-patching\" => true}} ";
+    echo "{\"extra\": {\"enable-patching\": true}} ";
     if (php_sapi_name() === 'cli') echo "\e[33m"; // Yellow
-    echo "in composer.json.\nWe have added it for you. Please run composer update/install again.\n";
+    echo "to composer.json.\nPlease run composer update/install again.";
     if (php_sapi_name() === 'cli') echo "\e[39m"; // Reset
-
-    // Unload this file
-    $composerJson = json_decode(file_get_contents(__DIR__ . '/composer.json'), true);
-    unset($composerJson['autoload']);
-    file_put_contents(__DIR__ . '/composer.json', json_encode($composerJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
-
-    exit(1);
 }
+
+// Unload this file
+$composerJson = json_decode(file_get_contents(__DIR__ . '/composer.json'), true);
+unset($composerJson['autoload']);
+file_put_contents(__DIR__ . '/composer.json', json_encode($composerJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+
+exit(1);
 
 // TODO: Maybe there is a better way to do this
 // Adding 'enable-patching' via a composer plugin runs too late
